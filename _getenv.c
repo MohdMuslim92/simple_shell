2a8acc9b3a7d4bc502e12e3dcf7630356dd1cb10
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-04-26 16:30:21 -0700

    Add customed getenv function, _getenv.c

---
+++ _getenv.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _getenv - search for name in env and return its value
+ * @name: pointer to the variable name
+ * @environ: environment variables pointer
+ * Return: pointer to name's value, or NULL if there no variable in name name
+ */
+char *_getenv(const char *name, char **environ)
+{
+	char *value;
+	int i, match;
+	unsigned int len, j;
+
+	if (name == NULL)
+	{
+		dprintf(STDOUT_FILENO, "%s: ", "MY PROMPT");
+		perror("EINVAL");
+		return (NULL);
+	}
+	len = strlen(name);
+	/*loop over all the environment variables searching for the name*/
+	i = 0;
+	while (environ[i] != NULL)
+	{
+		for (j = 0 ; j < len ; j++)
+		{
+			/*if the name did not match the current env break*/
+			if (name[j] != environ[i][j])
+			{
+				match = 0;
+				break;
+			}
+			match = 1;
+		}
+		if (match)
+		{
+			/*return the first '=' in the matched env*/
+			value = strpbrk(environ[i], "=");
+			if (value == NULL)
+				return (NULL);
+			/*++ because the returned pointer is including = sign*/
+			return (++value);
+		}
+		i++;
+	}
+	dprintf(STDOUT_FILENO, "%s: ", "MY PROMPT");
+	perror("EINVAL");
+	return (NULL);
+}
