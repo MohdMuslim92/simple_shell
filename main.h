f31187219d735fb48c417d88251e1219489d154a
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-04-26 16:27:28 -0700

    Add main header file of the project, main.h

---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <errno.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char *get_path(char *exefile, char **environ);
+char *_getenv(const char *name, char **environ);
+char *_strtok(char *str, char delim, unsigned int *len);
+
+#endif
