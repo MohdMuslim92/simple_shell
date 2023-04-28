4c2bd26ab197a902f197dc68fe717fef62a87c32
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-04-27 23:52:30 -0700

    Update main header file, edit function prototypes and add the extern variable environ

--- main.h
+++ main.h
@@ -14,8 +14,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *get_path(char *exefile, char **environ);
-char *_getenv(const char *name, char **environ);
+extern char **environ;
+
+char *get_path(char *exefile);
+char *_getenv(const char *name);
 char *_strtok(char *str, char delim, unsigned int *len);
 char *get_path2(char *exefile);
 int _strlen(char *s);
