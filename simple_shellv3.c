0a543b258062e4342bd093e9130e903b61c3ada5
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-04-28 18:37:26 -0700

    Edit main program to handle current and parent directories (path begins with . and ..), and modify error output

--- simple_shellv3.c
+++ simple_shellv3.c
@@ -2,6 +2,9 @@
 
 int _strcmp(char *str1, char *str2);
 int _empty(char *buffer);
+void _perror(char *prog_name, int err_nom, char *command);
+void print_num(int num);
+
 /**
  * main - entry point for the simple shell
  * @argc: arguments number
@@ -14,7 +17,7 @@ int main(__attribute__((unused))int argc, char **argv)
 
 	char *line = NULL, *exefile = NULL;
 	size_t arg_len = 0;
-	int i = 0, j = 0, pid = 0, status = 0, new_line = 0;
+	int i = 0, j = 0, pid = 0, status = 0, new_line = 0, err_nom = 1;
 	unsigned int len = 0;
 
 	do {
@@ -41,15 +44,21 @@ int main(__attribute__((unused))int argc, char **argv)
 		if (exefile == NULL)
 		{
 			free(line);
-			write(1, (void *)argv[0], _strlen(argv[0]));
-			write(1, ": ", 2);
+			write(2, (void *)argv[0], _strlen(argv[0]));
+			write(2, ": ", 2);
 			perror("");
 			continue;
 		}
-		if (_strcmp(exefile, "exit") == 0)
-			break;
-		if (exefile[0] == '/')
+		if (exefile[0] == '/' || exefile[0] == '.')
 			j = 1;
+		if (_strcmp(exefile, "exit") == 0)
+		{
+			free(exefile);
+			free(line);
+			if (len != 0)
+				exit(0);
+			exit(2);
+		}
 		args[0] = exefile;
 		i = 0;
 		while (args[i++] != NULL)
@@ -57,9 +66,8 @@ int main(__attribute__((unused))int argc, char **argv)
 		exefile = get_path(exefile);
 		if (exefile == NULL)
 		{
-			write(1, (void *)argv[0], _strlen(argv[0]));
-			write(1, ": ", 2);
-			perror("");
+			_perror(argv[0], err_nom, args[0]);
+			err_nom++;
 			free(line);
 			for (i = j ; args[i] ; i++)
 				free(args[i]);
@@ -83,8 +91,6 @@ int main(__attribute__((unused))int argc, char **argv)
 			free(args[i]);
 		free(line);
 	} while (1);
-	free(exefile);
-	free(line);
 	return (0);
 }
 
@@ -122,4 +128,37 @@ int _empty(char *buffer)
 			return (0);
 	}
 	return (1);
+}
+
+/**
+ * _perror - write error message to the stderror
+ * @prog_name: name of the program
+ * @err_nom: error number
+ * @command: name of the command
+ * Return: void
+ */
+void _perror(char *prog_name, int err_nom, char *command)
+{
+	write(2, (void *)prog_name, _strlen(prog_name));
+	write(2, ": ", 2);
+	print_num(err_nom);
+	write(2, ": ", 2);
+	write(2, command, _strlen(command));
+	write(2, ": not found\n", 12);
+}
+
+/**
+ * print_num - print an intger number n
+ * @num: intger number arg1
+ * Return: void
+ */
+
+void print_num(int num)
+{
+	if (num > 9)
+	{
+		print_num(num / 10);
+	}
+	num = num % 10 + '0';
+	write(2, &num, 1);
 }
