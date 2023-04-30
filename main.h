133e388a7d323c69033d203aad5c9c0f75b23dfe
Mojtaba-Babik3r <mojmohammed@gmail.com>
2023-04-30 16:26:54 -0700

    Update main header file, main.h impelementing _env() function

--- main.h
+++ main.h
@@ -18,6 +18,7 @@ extern char **environ;
 
 char *get_path(char *exefile);
 char *_getenv(const char *name);
+int _env(void);
 char *_strtok(char *str, char delim, unsigned int *len);
 char *get_path2(char *exefile);
 int _atoi(char *s);
