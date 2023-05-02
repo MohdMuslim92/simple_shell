26b092514032621105d98ebb7aa5647950d606d8
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-05-01 19:00:21 -0700

    Added function prototype for _cd()

--- main.h
+++ main.h
@@ -19,6 +19,7 @@ extern char **environ;
 char *get_path(char *exefile);
 char *_getenv(const char *name);
 int _env(void);
+int _cd(char *prog_name, char **args);
 char *_strtok(char *str, char delim, unsigned int *len);
 char *get_path2(char *exefile);
 int _atoi(char *s);
